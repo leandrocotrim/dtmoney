0ee19acc6bbeb06152c2da72da13fe38670892f6
Leandro Cotrim <cotrim.leandro@gmail.com>
2021-07-29 01:46:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {GlobalStyle} from './styles/global';
+import { Header } from './components/header';
+
+export function App() {
+  return (
+  <>
+    <Header/>
+    <GlobalStyle/>
+  </>
+  )
+}
